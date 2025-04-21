2392b4aaffd5a2a5038b2d390547563e850fc5cf
cheerlasaikiran <saicheerla2002@gmail.com>
2025-04-21 12:39:39 +0000

    changed test case name

--- Project/src/test/java/runner/TestSample.java
+++ Project/src/test/java/runner/TestSample.java
@@ -1,5 +1,35 @@
 package runner;
 
-public class TestSample {
-    
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import utils.Base;
+import utils.Reporter;
+
+public class TestSample extends Base{
+    public static ExtentTest test;
+    public static ExtentReports report;
+    @BeforeClass
+    public void createReport(){
+        report=Reporter.generateExtentReport("ELC_Report");
+    }
+    @BeforeMethod
+    public void openELC(){
+        openBrowser();
+        driver.navigate().refresh();
+    }
+    @AfterMethod
+    public void closeELC(){
+        driver.quit();
+    }
+    @AfterClass
+    public void closeReport(){
+        report.flush();
+    }
+
 }
