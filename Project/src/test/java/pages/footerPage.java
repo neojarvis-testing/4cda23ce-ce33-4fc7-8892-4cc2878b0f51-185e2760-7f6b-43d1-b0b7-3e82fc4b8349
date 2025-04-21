c4619d588a37888ed2f921cb7434983c28fc2ff4
cheerlasaikiran <saicheerla2002@gmail.com>
2025-04-21 13:26:42 +0000

    latestUpdate

--- Project/src/test/java/pages/footerPage.java
+++ Project/src/test/java/pages/footerPage.java
@@ -1,5 +1,44 @@
 package pages;
 
+import org.testng.Assert;
+
+import com.aventstack.extentreports.ExtentTest;
+
+import uistore.HomePageLocators;
+import utils.Base;
+import utils.WebDriverHelper;
+
 public class footerPage {
-    
+    public static ExtentTest test;
+    public static WebDriverHelper helper;
+    public footerPage(ExtentTest test){
+        this.test=test;
+        helper=new WebDriverHelper(Base.driver);
+    }
+    public void clickOnAcceptCookies(){
+        try {
+            helper.wait(HomePageLocators.Cookies);
+            helper.clickOnElement(HomePageLocators.Cookies);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void CLickOnAboutUs(){
+        try {
+            helper.jsScrollIntoView(HomePageLocators.aboutUs);
+            helper.wait(HomePageLocators.aboutUs);
+            helper.clickOnElement(HomePageLocators.aboutUs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void verifyAboutUsUrl(){
+        try {
+            String value=helper.getUrl();
+            Assert.assertEquals(value, value);
+            System.out.println(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
